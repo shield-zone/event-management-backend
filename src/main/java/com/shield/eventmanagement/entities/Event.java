46705b19a91c51ef43b6d51d618f9406db220088
GedoRinneRebirth <59303347+GedoRinneRebirth@users.noreply.github.com>
2023-03-22 21:11:52 +0530

    Updated Event.java

--- src/main/java/com/shield/eventmanagement/entities/Event.java
+++ src/main/java/com/shield/eventmanagement/entities/Event.java
@@ -1,5 +1,40 @@
 package com.shield.eventmanagement.entities;
 
-public class Event {
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Event {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long eventId;
+	
+	private String eventName;
+	
+	private String eventType;
+	
+	private String startDate;
+	
+	private String endDate;
+	
+	private Double eventPrice;
+	
+	@ManyToMany
+	private List<Organizer> organizers = new ArrayList<Organizer>();
+	
 }
+

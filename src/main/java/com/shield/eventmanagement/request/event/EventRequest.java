3211c549880e22eed53d6ad6e15d7d3576886721
Ritik Ranjan <rritik772@outlook.com>
2023-03-29 00:30:41 +0530

    added LocationRequest and EventRequest for creating each one

--- src/main/java/com/shield/eventmanagement/request/event/EventRequest.java
+++ src/main/java/com/shield/eventmanagement/request/event/EventRequest.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
 @Data
@@ -14,4 +15,19 @@ public class EventRequest {
     @NotNull
     private Long eventId;
 
+    @NotEmpty
+    private String endDate;
+
+    @NotEmpty
+    private String startDate;
+
+    @NotEmpty
+    private String eventName;
+    @NotNull
+    private double eventPrice;
+
+    @NotNull
+    private Long locationId;
+    @NotNull
+    private Long organizerId;
 }

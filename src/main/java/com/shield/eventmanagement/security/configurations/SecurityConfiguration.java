a6e7157e86101bbdfd8652fa3aa8a3c8fe5d8980
Ritik Ranjan <rritik772@outlook.com>
2023-03-29 10:05:15 +0530

    resolved cors error for axios

--- src/main/java/com/shield/eventmanagement/security/configurations/SecurityConfiguration.java
+++ src/main/java/com/shield/eventmanagement/security/configurations/SecurityConfiguration.java
@@ -16,6 +16,11 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 import com.shield.eventmanagement.security.filter.JwtFilter;
 import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
@@ -31,10 +36,22 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 		auth.userDetailsService(userDetailsService);
 	}
 
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOrigins(Collections.singletonList("*"));
+		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+		configuration.setAllowedHeaders(Collections.singletonList("*"));
+		configuration.setExposedHeaders(Arrays.asList("Authorization", "content-type"));
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
+	}
+
 	protected void configure(HttpSecurity http) throws Exception {
 		http
-			.cors()
-			.configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues());
+			.cors();
 
 		http.csrf()
 			.disable()
